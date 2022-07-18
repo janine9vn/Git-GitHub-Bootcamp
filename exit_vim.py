735a80955d09483967ee7477cc02a96c001c1cc2
Alfonzitos <nylenalfons@gmail.com>
2022-07-18 23:27:34 +0200

    Exiting Vim
    Short and simple ways to exit vim :)

---
+++ exit_vim.py
@@ -0,0 +1,10 @@
+def main():
+    print("To quit vim: ")
+    print("1: [ctrl+c] then :wq to save changes")
+    print("2: [ctrl+c] then :q! to discard changes")
+    print("3: [ctrl+c] then :q if no changes were made")
+
+    print("I am now saving this file by using the first option!")
+
+if __name__ == "__main__":
+    main()
