d6965f6bce3749c2a0770bbb77f6509e24d32eb2
Ast3risk-ops <spacedude609@pm.me>
2022-07-18 12:20:59 -0400

    testing making branches for commits with hello_mina.py
    Whoops, forgot delays

--- hello_mina.py
+++ hello_mina.py
@@ -1,13 +1,18 @@
 import os
-print("Mina says hi")
+from time import sleep
 
+print("Mina says hi")
+sleep(1)
 # did tests, etc on my branch and now i'm happy.
 print("Girard says hi as well!")
-
+sleep(3)
 print("Ast3risk says hi!")
+sleep(1)
 print("*lists your system components*")
+sleep(3)
 # Only works on Linux
 os.system('neofetch && tree') # Would prefer exa but most don't have it
+sleep(3)
 
 # Windows variety, no neofetch for win.
 # os.system('tree')
