3a637d5e9884e96e780e4c8f78f0d7cb956bc142
Ast3risk-ops <spacedude609@pm.me>
2022-07-18 12:11:29 -0400

    Why not say more than hello?

--- hello_mina.py
+++ hello_mina.py
@@ -1,4 +1,13 @@
+import os
 print("Mina says hi")
 
 # did tests, etc on my branch and now i'm happy.
 print("Girard says hi as well!")
+
+print("Ast3risk says hi!")
+print("*lists your system components*")
+# Only works on Linux
+os.system('neofetch && tree') # Would prefer exa but most don't have it
+
+# Windows variety, no neofetch for win.
+# os.system('tree')
